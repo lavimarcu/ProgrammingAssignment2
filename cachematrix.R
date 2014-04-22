04e33d44e8534e2af212d434f8c0fddd22ef6306
lavimarcu <lavimarcu@gmail.com>
2014-04-22 13:20:45 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,35 @@
-## The first function creates a matrix by taking 3 arguments - x as the actual content of the matrix,y as the number
-##of rows and z as the number of columns. 
-## The second function takes an argument variable that is specified by the user - that should be a matrix.
-##Next, the function searches in cache the matrix created by makeCacheMatrix function. If the matrix
-##is found in memory and if the variable that was input by user is a matrix that is similar with the cached one
-##and if the inverse of the cached matrix is not null, the function takes the inverse matrix from cache and 
-##displays it. Alternative, it computes and prints the inverse of the matrix. 
+## The first function creates a matrix that is stored in R cache.  
+## The second function gets the value of the matrix, checks if it similar with the cached matrix and checks if the inverse
+##has been calculated. If the matrixes are similar and the inverse is already calculated, it retrives the inverse
+##from cahce. Otherwise, it calculates and displays the inverse. 
 
-## This function takes three arguments from the user and 1) creates the matrix and assigns it to variable "m"
-## 2) calculates m's inverse by using "solve" function. 
+## This function takes an user matrix argument; sets its value; gets the value; sets its inverse; gets the inverse.
 
-##makeCacheMatrix <- function(x = matrix()) {
-
-makeCacheMatrix <- function(x,y,z) {
-        m <<- matrix(x,nrow=y,ncol=z)
-        #m_inv <<- matrix()
-        m_inv <<- solve(m)
+makeCacheMatrix <- function(x=matrix()){
+	inv <- NULL
+	set <- function(y){
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(solve) inv <<- solve
+	getinv <- function() inv
+	list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
+## This function takes a matrix argument; checks if the matrix is similar with the one created with previous function; 
+##if similar and the inverse is already calculated, it prints the inverse from cache. Otherwise, it computes and prints
+## the inverse. 
 
-## Write a short comment describing this function
-
-##cacheSolve <- function(x, ...) {
-## This function computes the inverse of the matrix constructed by previous function if it cannot find the inverse
-##in memory(cache).
-
-cacheSolve <- function(x) {
-                     new <- get("m",inherits=TRUE)
-         
-         if (identical(new,x) & !is.na(m_inv)){
-         	print("getting cached data")
-         	#return(m_inv)
-         }     else {
-         	m_inv <- solve(m)
-         }
-         m_inv
+cachesolve <- function(x, ...) {
+        p <- x$get()
+        inv <- x$getinv()
+        if(identical(x,p) & !is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        matrix <- x$get()
+        inv <- solve(matrix, ...)
+        x$setinv(inv)
+        inv
 }
